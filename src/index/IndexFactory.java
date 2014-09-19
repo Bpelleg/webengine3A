f5817633aa407a2001fd434f31ca5799c5d77cc8
Pellegrini <baptiste.pellegrini@ecole.ensicaen.fr>
2014-09-19 12:15:06 +0200

    computation of the TF-IDFs

--- src/index/IndexFactory.java
+++ src/index/IndexFactory.java
@@ -3,7 +3,10 @@
  */
 package index;
 
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author pellegrini
@@ -22,9 +25,65 @@ public class IndexFactory {
 	 * @param filesContents
 	 */
 	
-	public Index buildIndex(List<String> filesContents){
-		//TODO implement
-		return null;
+	public Index buildIndex(Map<String,String> files){
+		Map<String,Map<String,Float>> indexMap=new HashMap<String,Map<String,Float>>();
+		Map<String,Integer> wordOccurencesInFiles=new HashMap<String,Integer>();
+		int nbFiles=files.keySet().size();
+		
+		for(String doc:files.keySet()){
+			String fileContent=files.get(doc);
+			Map<String,Float> docIndex=new HashMap<String,Float>();
+			//get all the words in an array
+			//the regular expression detects punctuation and white blanks in the text
+			String [] words=fileContent.split("[\\p{Punct}\\s]+");
+			for(String word:words){
+				if(isValid(word)){
+					if(!docIndex.containsKey(word)){
+						docIndex.put(word, Float.valueOf(0));
+					}
+					docIndex.put(word, docIndex.get(word)+1);
+				}
+			}
+			
+			for(String word:docIndex.keySet()){
+				//update wordOccurrencesInFiles (dfi)
+				if(!wordOccurencesInFiles.containsKey(word)){
+					wordOccurencesInFiles.put(word, Integer.valueOf(0));
+				}
+				wordOccurencesInFiles.put(word, wordOccurencesInFiles.get(word)+1);
+			}
+			
+			// at this step the scores are the number of words' occurences in the file
+		indexMap.put(doc, docIndex);
+		}
+		System.out.println(indexMap);
+		System.out.println(wordOccurencesInFiles);
+		//computes the TF-IDFs
+		for(String doc:indexMap.keySet()){
+			Map<String,Float> docMap=indexMap.get(doc);
+			for(String word:indexMap.get(doc).keySet()){
+				float tfidf=tfIdf(docMap.get(word), nbFiles, wordOccurencesInFiles.get(word));
+				docMap.put(word, Float.valueOf(tfidf));
+			}
+		}
+		System.out.println(indexMap);
+		Index index=new Index();
+		index.setIndex(indexMap);
+		index.setNbDoc(nbFiles);
+		return index;
+	}
+
+	private boolean isValid(String word) {
+		// TODO Auto-generated method stub
+		if(word.length()<3){
+			return false;
+		}
+		return true;
+	}
+	
+	private float tfIdf(float tfi,int N, int dfi){
+		float log=(float) Math.log(((double)N)/((double)dfi));
+		return tfi*log;
 	}
 	
 
